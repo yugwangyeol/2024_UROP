75c0c408469f4e633c0d9f8da01783247c3445a7
yugwangyeol <rhkdduf627@naver.com>
2025-01-05 15:41:14 +0900

    upload Discriminator Conv2d

---
+++ Discriminator_Conv2d/main.py
@@ -0,0 +1,47 @@
+import torch
+import torchaudio
+from torch.utils.data import DataLoader
+from transformers import WavLMModel
+from model import Discriminator, Generator
+from train import train_noise_encoder
+import os
+# 장치 설정
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# 모델 저장 경로 설정
+save_dir = './checkpoints'
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+# 하이퍼파라미터
+batch_size = 4
+num_epochs = 10
+learning_rate = 0.001
+lambda_emb = 0.2
+lambda_wav = 0.8
+# 데이터 로드
+train_dataset = torchaudio.datasets.VCTK_092(root="../../Data", download=False)
+def collate_fn(batch):
+    waveforms = []
+    max_length = max(waveform.shape[1] for waveform, sample_rate, utterance, speaker_id, utterance_id in batch)
+    for waveform, sample_rate, utterance, speaker_id, utterance_id in batch:
+        if waveform.shape[1] < max_length:
+            padding = torch.zeros(1, max_length - waveform.shape[1])
+            waveform = torch.cat([waveform, padding], dim=1)
+        waveforms.append(waveform)
+    return torch.stack(waveforms)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+# 모델 초기화
+wavlm = WavLMModel.from_pretrained("microsoft/wavlm-large").to(device)
+generator = Generator().to(device)
+discriminator = Discriminator().to(device)
+# 모델 초기화
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=learning_rate)
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=learning_rate)
+# 학습 실행
+print("train Discriminator conv2d")
+img_losses, emb_losses, gan_losses = train_noise_encoder(generator, discriminator, wavlm, train_loader, optimizer_G, optimizer_D, num_epochs, batch_size, device, lambda_wav, lambda_emb)
+# 학습 완료 메시지 출력
+print("학습 완료!")
+# 모델 저장
+torch.save(generator.state_dict(), os.path.join(save_dir, 'generator.pth'))
+torch.save(discriminator.state_dict(), os.path.join(save_dir, 'discriminator.pth'))
+print(f"모델이 {save_dir}에 저장되었습니다.")
